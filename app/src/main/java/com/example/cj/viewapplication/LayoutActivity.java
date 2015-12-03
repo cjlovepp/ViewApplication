ffb5d0769e834dd72e451ea00a0e455474e24d52
cjlovepp <cjlovepp@gmail.com>
2015-12-03 11:46:19 +0800

    相对布局使用

--- app/src/main/java/com/example/cj/viewapplication/LayoutActivity.java
+++ app/src/main/java/com/example/cj/viewapplication/LayoutActivity.java
@@ -2,39 +2,23 @@ package com.example.cj.viewapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-
-public class CustomActivity extends Activity {
+public class LayoutActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_custom);
-
-        //一个简单的循环，用来学习使用AS断点调试
-        for (int i = 0; i < 10; i++) {
-            //获取当前i的值
-            int selector = i;
-            //打log查看当前i的值（此步多余，实际开发请忽略）
-            Log.e(this.getPackageName(),"当前循环"+i);
-            stepNext(i);
-
-        }
-    }
-
-    public void stepNext(int num){
-        Log.i(this.getPackageName(), "info"+num);
+        setContentView(R.layout.relativelayout);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_custom, menu);
+        getMenuInflater().inflate(R.menu.menu_layout, menu);
         return true;
     }
 
